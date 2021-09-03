8a5954595e84dc4ab200050c2d8f2c67e4786036
Md Riaj Khan <71939620+webriaj@users.noreply.github.com>
2021-09-03 10:56:27 +0600

    change

---
+++ app.js
@@ -0,0 +1,27 @@
+// var date = new Date();
+// var time = date.getDay();
+
+
+// switch (time){
+//     case 0: 
+//         console.log('SunDay')
+//         break;
+//     case 1:
+//         console.log('MonDay')
+//         break;
+//     case 2:
+//         console.log('TuesDay')
+//         break;
+//     case 3: 
+//         console.log('WednesDay')
+//         break;
+// }
+
+
+
+function test(){
+    var num = document.getElementById('vNum').value;
+    var result = Math.sqrt(num);
+    document.getElementById('res').createElement = result;
+}
+
